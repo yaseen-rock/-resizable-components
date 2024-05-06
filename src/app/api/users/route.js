9a768d53e6dd1f6e612c15cfb4486ccf23fd88c8
yaseen-rock <yaseen.ragib@gmail.com>
2024-05-06 11:31:10 +0530

    added responsiveness

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,71 +5,73 @@ connectDb();
 
 export async function GET(request) {
   try {
-      const users = await User.find(); // Assuming User.find() returns all users
-      return NextResponse.json(users, { status: 200 });
+    const users = await User.find(); // Assuming User.find() returns all users
+    return NextResponse.json(users, { status: 200 });
   } catch (error) {
-      console.error(error);
-      return NextResponse.json(
-          {
-              message: "Failed to fetch users",
-              status: false,
-          },
-          {
-              status: 500,
-          }
-      );
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "Failed to fetch users",
+        status: false,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
 
 export async function PUT(request) {
-    try {
-        const { id, about } = await request.json(); // Assuming you're passing user ID and about in the request body
-        const updatedUser = await User.findByIdAndUpdate(id, { about }, { new: true }); // Update the user with the provided ID and about field
-        return NextResponse.json(updatedUser, { status: 200 });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json(
-            {
-                message: "Failed to update user",
-                status: false,
-            },
-            {
-                status: 500,
-            }
-        );
-    }
+  try {
+    const { id, about } = await request.json(); // Assuming you're passing user ID and about in the request body
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { about },
+      { new: true }
+    ); // Update the user with the provided ID and about field
+    return NextResponse.json(updatedUser, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "Failed to update user",
+        status: false,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 
 export async function POST(request) {
-    // fetch user detail from  request
-  
-    const { about } = await request.json();
-  
-    console.log({ about });
-  
-    // create user object with user model
-  
-    const user = new User({
-     
-      about
+  // fetch user detail from  request
+
+  const { about } = await request.json();
+
+  console.log({ about });
+
+  // create user object with user model
+
+  const user = new User({
+    about,
+  });
+  try {
+    const createdUser = await user.save();
+    const response = NextResponse.json(user, {
+      status: 201,
     });
-    try {
-       
-        const createdUser = await user.save();
-        const response = NextResponse.json(user, {
-          status: 201,
-        });
-        return response;
-      } catch (error) {
-        console.log(error);
-        return NextResponse.json(
-          {
-            message: "failed to create user !!",
-            status: false,
-          },
-          {
-            status: 500,
-          }
-        );
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "failed to create user !!",
+        status: false,
+      },
+      {
+        status: 500,
       }
+    );
+  }
 }
